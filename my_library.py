8764c111f1f581b8c38c9cf95a2901da9f030d0a
Leisan <fleisan18@mail.ru>
2023-02-12 11:35:57 +0500

    2.1 task with Poisson_distribution

--- my_library.py
+++ my_library.py
@@ -1,8 +1,16 @@
-from math import factorial
+from math import factorial, e
 
 def combinations (k, n):
     return factorial(n) // (factorial(k) * factorial(n-k))
 
 def arrangements (k, n):
     return factorial(n) // factorial (n-k)
+
+def Bernoulli (k, n, p):
+    return combinations(k, n) * p**k * (1-p)**(n-k)
+
+def Poisson_distribution (k, n, p):
+    l = n * p
+    return l**k * e**(-l) / factorial(l) 
+
 
