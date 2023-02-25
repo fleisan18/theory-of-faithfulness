72c60f7d4711c30c45a6882c5795178843eb390c
Leisan <fleisan18@mail.ru>
2023-02-25 12:35:34 +0500

    3 task

--- home_task_6.py
+++ home_task_6.py
@@ -27,3 +27,11 @@ print(l, u)
 # 3) Рост дочерей 175, 167, 154, 174, 178, 148, 160, 167, 169, 170
 # Рост матерей 178, 165, 165, 173, 168, 155, 160, 164, 178, 175
 # Используя эти данные построить 95% доверительный интервал для разности среднего роста родителей и детей.
+mothers = [178, 165, 165, 173, 168, 155, 160, 164, 178, 175]
+daughters = [175, 167, 154, 174, 178, 148, 160, 167, 169, 170]
+
+delta = np.mean(mothers) - np.mean(daughters)
+D = (np.var(mothers, ddof=1) + np.var(daughters, ddof=1)) / 2
+se = (D / len(mothers) + D / len(daughters))**0.5
+(l, u) = (delta - stats.t.ppf(0.975, len(mothers) + len(daughters) - 2) * se, delta + stats.t.ppf(0.975, len(mothers) + len(daughters) - 2) * se)
+print(l, u)
